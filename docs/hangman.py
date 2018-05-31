a6d50c9877c56ed24b46b2765dfced6fc1ae2114
Pedro mate <36180116+pedrooste@users.noreply.github.com>
2018-05-31 10:22:47 +1000

    updated 31/5/18 10:19
    added 
    .strip to get rid of the /n
    wrong guesses are displayed
    info up top
    general tidy up on how data is displayed
    needed: (cant figure it out)
    - win the game, (if word complete)
    - join the correct guesses in order
    - loop the correct guesses as well? also the blank spaces?
    
    GL keep me updated

--- docs/hangman.py
+++ docs/hangman.py
@@ -1,9 +1,22 @@
+""" Hangman game to be played in dentist wating room.
+This code is designed to be played as a text based hangman game, prototype to the GUI game which will be created later. The hangman
+is to be replaced with lives.
+"""
+
+__author__ = "Tynan, Pedro and Nicole"
+__copyright__ = "Copyright 2018, NSW Dept. Ed."
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Tynan, Pedro and Nicole"
+__email__ = "tynan.matthews@education.nsw.com.au"
+__status__ = "Prototype" 
+
+
 import random as r
 import time
 #variables
 lives = 12
-wrong = 0
-wrongGuess = []
+wrongGuess = ''
 
 #n stands for number of words in text document
 try:
@@ -15,22 +28,18 @@ except IOError:
 
 #determine word & test
 word = r.choice(wordList)
+word = word.strip()
+#strips the /n off the end so the word length is correct
 print(word)
 
 #determine word length & test
-wordLength = len(word)-1
+wordLength = len(word)
 print(wordLength)
 
+print('hint: Animals')
 #print blank space
 blankSpace = "_ " * wordLength
 print(blankSpace)
-
-#print lives
-displayLives = "O" * lives
-print(displayLives)
-
-#experimental
-
 
 """
 wrongGuess.append(guess)
@@ -39,16 +48,18 @@ wrongGuess.append(guess)
 print("Previous Guesses ", wrongGuess)
 """
 while lives >= 0:
-    guess = input("Guess a letter  ")
-    displayLives = "O" * lives
+    #print lives
+    displayLives = 'lives: '+("O" * lives)
     print(displayLives)
+    guess = input("Guess a letter  ")
     if guess in word:
       print("yesi")
       print(guess)
     else:
       print("no")
       lives = lives-1
-      wrongGuess = ''+ guess
+      #shows all wrong guesses
+      wrongGuess = wrongGuess+' '+ guess
       print('wrong guesses: '+wrongGuess)
       
 print("you loose")
